8d58a6808085fdf91541d9a204b9dc76d22f66da
БаймахановаК <@234_4>
2018-05-29 12:06:50 +0600

    Add project files.

---
+++ Module1Lesson2/Source.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+		int sum = 0, month = 0, p = 5, b = 365, c = 30;
+
+	printf("Введите желаемую сумму депозита: ");
+	scanf("%d", &sum);
+
+	printf("На какое количество месяцев: ");
+	scanf("%d", &month);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+		//int a = 1, b = 13, c = 49; 
+
+	//printf("%d \t", a);
+	//printf("%d \t", b);
+	//printf("%d \t \n", c);
+
+	//float n = 3.14;
+	//float R = 2, r = 5, S=0;
+
+	//S = n * (R*R - r*r);
+
+	//printf("Площадь круга = %f \n ", S);	
+	
+	//int a = 3, b = 4, c = 0, S = 0;
+	//
+	//c = (a*a + b*b);
+	//S = a*b;
+
+	//printf("Гипотенуза = %d \n", c);
+	//printf("Площадь треугольника = %d \n", S);
+	
+	/*	int a = 0, b = 0, c=0;
+	printf("Введите первое число: ");
+	scanf("%d", &a);
+
+	printf("Введите второе число: ");
+	scanf("%d", &b);
+
+	c = (a + b) / 2;
+
+	printf("Среднее арифметическое = %d\n", c);
+*/
+
+	//int a = 0, b = 0, sum =0, r =0, p = 0;
+
+	//printf("Введите первое число: ");
+	//scanf("%d", &a); 
+
+	//printf("Введите второе число: ");
+	//scanf("%d", &b);
+
+	//sum = a + b;
+	//r = a - b;
+	//p = a * b;
+
+	//printf("Сумма = %d \n", sum);
+	//printf("Разность = %d \n", r);
+	//printf("Произведение = %d \n", p);
+
+
+	//// Известны год и номер месяца рождения человека. 
+	//// Определить возраст человека.
+	//int month = 0;
+	//int year = 0;
+	//int day = 0;
+	//int age = 0;
+
+	//printf(" Введите год рождения : ");
+	//scanf("%d", &year);
+
+	//printf(" Введите месяц рождения : ");
+	//scanf("%d", &month);
+
+	//printf(" Введите день рождения : ");
+	//scanf("%d", &day);
+
+	//age = 2018 - year; 
+	//printf("возраст = %d \n", age);
+
